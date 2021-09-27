be58021b1c431d8bead55c78bf6620cb96ec2acb
Peter Murphy <peter@spantree.net>
2021-09-27 11:28:50 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,94 @@
-import logo from './logo.svg';
+import { useActor } from '@xstate/react';
+import _ from 'lodash';
+import { assign, createMachine, interpret } from 'xstate';
+
 import './App.css';
 
+const NUM_FIELDS = 100;
+const NUM_ROWS = 1200;
+
+function generateData() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const rows = [];
+      for (let i = 0; i < NUM_ROWS; i++) {
+        const row = {};
+        for (let j = 0; j < NUM_FIELDS; j++) {
+          row[_.uniqueId('field')] = Math.random();
+        }
+        rows.push(row);
+      }
+      resolve({ rows });
+    }, 3000);
+  });
+}
+
+const machine = createMachine(
+  {
+    key: 'test',
+    initial: 'initial',
+    strict: true,
+    preserveActionOrder: true,
+    context: {
+      data: [],
+    },
+    states: {
+      initial: {
+        after: {
+          INITIAL_WAIT: {
+            target: 'active',
+          },
+        },
+      },
+
+      active: {
+        after: {
+          POLLING_INTERVAL: {
+            target: 'fetching',
+          },
+        },
+      },
+
+      fetching: {
+        invoke: {
+          src: 'loadData',
+          onDone: {
+            target: 'active',
+            actions: 'updateData',
+          },
+        },
+      },
+    },
+  },
+  {
+    delays: {
+      INITIAL_WAIT: 2000,
+      POLLING_INTERVAL: 10000,
+    },
+
+    actions: {
+      updateData: assign({
+        data: (_, { data }) => data.rows,
+      }),
+    },
+
+    services: {
+      loadData: async () => await generateData(),
+    },
+  },
+);
+
+export const actor = interpret(machine);
+actor.start();
+
 function App() {
+  const [current] = useActor(actor);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>XState Memory Test</p>
+        <pre>{JSON.stringify(current.value)}</pre>
+        {/* <pre>{JSON.stringify(current.context, null, 3)}</pre> */}
       </header>
     </div>
   );
